a7fca3819b4402b9a98fceb29e56f0be94ab83cd
stanislavn <29331439+stanislavn@users.noreply.github.com>
2023-02-07 20:51:01 +0100

    first awesome commit

---
+++ backbone/backbone/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from backbone.users.models import User
+from backbone.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
